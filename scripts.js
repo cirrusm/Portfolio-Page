d50dc8e36b50188a054b8738e9d45a21d6006dd5
Cirrus Mokhtari <Cirrus1994@gmail.com>
2020-12-13 12:05:50 -0800

    landing page area animations added

--- scripts.js
+++ scripts.js
@@ -14,7 +14,7 @@ navBar.classList.add("show")
   }
 });
 
-navOpen.addEventListener("click", () => {
+navClose.addEventListener("click", () => {
   if (navLeft < 0) {
     menu.classList.remove("show");
 document.body.classList.remove("show");
@@ -26,14 +26,40 @@ navBar.classList.remove("show")
 
 new TypeIt('#type1', {
   speed: 160,
-  loop: false,
+  loop: true,
   waitUntilVisible: true,
-}).type('Software Engineer', { delay: 400})
-.pause(1300)
+})
+.pause(3000)
+.type('Software Engineer', { delay: 400})
+.pause(2400)
 .delete(18)
 .type('Full-Stack Developer', {delay: 400})
-.pause(1300)
+.pause(2400)
 .delete(28)
 .type('UCSB Graduate')
-
 .go()
+
+//GSAP
+
+gsap.from('.logo', {opacity: 0, duration: 1, delay: 0.5, y: -10})
+gsap.from('.hamburger', {opacity: 0, duration: 1, delay: 1, x: 20})
+gsap.from('.banner', {opacity: 0, duration: 1, delay: 1.5, x: -200})
+gsap.from('.hero h3', {opacity: 0, duration: 1, delay: 2, y: -50})
+gsap.from('.hero h1', {opacity: 0, duration: 1, delay: 2.5, y: -45})
+gsap.from('.hero h4', {opacity: 0, duration: 1, delay: 3, y: -30})
+gsap.from('.hero a', {opacity: 0, duration: 1, delay: 3, y: 50})
+gsap.from(".nav-item", {
+  opacity: 0,
+  duration: 1,
+  delay: 1.2,
+  y: 30,
+  stagger: 0.2
+})
+
+gsap.from(".icons span", {
+  opacity: 0,
+  duration: 1,
+  delay: 5.5,
+  y: 30,
+  stagger: 0.2
+})
